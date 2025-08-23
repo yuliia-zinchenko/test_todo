5c423dfdbef0d5da33d3435878e6209fecf16018
Yulia <julietta.zinchenko@gmail.com>
2025-08-23 19:04:30 +0300

    new PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,10 @@ import buildApp from "./app.js";
 
 const start = async () => {
   const app = buildApp();
+  const PORT = process.env.PORT || 5000;
 
   try {
-    await app.listen({ port: 5000 });
-    console.log("server is running on http://localhost:5000 ");
+    await app.listen({ port: Number(PORT), host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
